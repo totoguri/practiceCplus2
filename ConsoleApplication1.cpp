facc59948946778fe061f667b5d292f7968b0d86
totoguri <finalyo4@naver.com>
2021-09-19 19:21:17 +0900

    Cplus

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -3,8 +3,59 @@
 
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+//int main()
+//{
+//    int num = 20;
+//    std::cout << "Hello World!" << std::endl;
+//    std::cout << "Hello " << "World!" << std::endl;
+//    std::cout << num << ' ' << 'A';
+//    std::cout << ' ' << 3.14 << std::endl;
+//    return 0;
+//}
 
+
+//int main() {
+//	int val1;
+//	std::cout << "First Num: ";
+//	std::cin >> val1;
+//
+//	int val2;
+//	std::cout << "Second Num: ";
+//	std::cin >> val2;
+//
+//	int result = val1 + val2;
+//	std::cout << "Result: " << result << std::endl;
+//	return 0;
+//}
+
+
+//void MyFunc(void) {
+//	std::cout << "MyFunc(void) called" << std::endl;
+//}
+//void MyFunc(char c) {
+//	std::cout << "MyFunc(char c) called" << std::endl;
+//}
+//void MyFunc(int a, int b) {
+//	std::cout << "MyFunc(int a, int b) called" << std::endl;
+//}
+//
+//int main() {
+//	MyFunc();
+//	MyFunc('A');
+//	MyFunc(12, 13);
+//	return 0;
+//}
+
+
+//inline int SQUARE(int x) {
+//	return x * x;
+//}
+//
+//int main() {
+//	std::cout << SQUARE(5) << std::endl;
+//	std::cout << SQUARE(12) << std::endl;
+//	return 0;
+//}
+
+
+// p.34
