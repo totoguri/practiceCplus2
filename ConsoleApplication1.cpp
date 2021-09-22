97aa8e8600e6ce4877657517ef10752c4a0f9637
totoguri <finalyo4@naver.com>
2021-09-22 09:57:26 +0900

    cplus

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -182,3 +182,164 @@ using namespace std;
 //}
 
 // p.87
+
+
+//#include <string.h>
+//#include <stdlib.h>
+//
+//char* MakeStrAdr(int len) {
+//	char* str = (char*)malloc(sizeof(char) * len);
+//	return str;
+//}
+//
+//int main() {
+//	char* str = MakeStrAdr(20);
+//	strcpy_s(str, 20, "I am so happy");
+//	cout << str << endl;
+//	free(str);
+//	return 0;
+//}
+
+
+//#include <string.h>
+//char* MakeStrAdr(int len) {
+//	char* str = new char[len];
+//	return str;
+//}
+//
+//int main(void) {
+//	char* str = MakeStrAdr(20);
+//	strcpy_s(str, 20, "I am so happy");
+//	cout << str << endl;
+//	delete[]str;
+//	return 0;
+//}
+
+
+//#include <cstring>
+//
+//namespace CAR_CONST {
+//	enum {
+//		ID_LEN = 20,
+//		MAX_SPD = 200,
+//		FUEL_STEP = 2,
+//		ACC_STEP = 10,
+//		BRK_STEP = 10
+//	};
+//}
+//
+//class Car {
+//private:
+//	char gamerID[CAR_CONST::ID_LEN];
+//	int fuelGauge;
+//	int curSpeed;
+//public:
+//	void InitMembers(const char* ID, int fuel);
+//	void ShowCarState();
+//	void Accel();
+//	void Break();
+//};
+//
+//void Car::InitMembers(const char* ID, int fuel) {
+//	strcpy_s(gamerID, 20, ID);
+//	fuelGauge = fuel;
+//	curSpeed = 0;
+//}
+//void Car::ShowCarState() {
+//	cout << "소유자ID: " << gamerID << endl;
+//	cout << "연료량: " << fuelGauge << "%" << endl;
+//	cout << "현재속도: " << curSpeed << "km/s" << endl << endl;
+//}
+//void Car::Accel() {
+//	if (fuelGauge <= 0)
+//		return;
+//	else
+//		fuelGauge -= CAR_CONST::FUEL_STEP;
+//
+//	if ((curSpeed + CAR_CONST::ACC_STEP) >= CAR_CONST::MAX_SPD) {
+//		curSpeed = CAR_CONST::MAX_SPD;
+//		return;
+//	}
+//	curSpeed += CAR_CONST::ACC_STEP;
+//}
+//void Car::Break() {
+//	if (curSpeed < CAR_CONST::BRK_STEP) {
+//		curSpeed = 0;
+//		return;
+//	}
+//	curSpeed -= CAR_CONST::BRK_STEP;
+//}
+//
+//int main() {
+//	Car run99;
+//	run99.InitMembers("run99", 100);
+//	run99.Accel();
+//	run99.Accel();
+//	run99.Accel();
+//	run99.ShowCarState();
+//	run99.Break();
+//	run99.ShowCarState();
+//	return 0;
+//}
+
+
+class FruitSeller
+{
+private:
+	int APPLE_PRICE;
+	int numOfApples;
+	int myMoney;
+
+public:
+	void InitMembers(int price, int num, int money) {
+		APPLE_PRICE = price;
+		numOfApples = num;
+		myMoney = money;
+	}
+	int SaleApples(int money) {
+		int num = money / APPLE_PRICE;
+		numOfApples -= num;
+		myMoney += money;
+		return num;
+	}
+	void ShowSalesResult() {
+		cout << "Left Apples: " << numOfApples << endl;
+		cout << "Earned Money: " << myMoney << endl << endl;
+	}
+};
+
+class FruitBuyer {
+private:
+	int myMoney;
+	int numOfApples;
+
+public:
+	void InitMembers(int money) {
+		myMoney = money;
+		numOfApples = 0;
+	}
+	void BuyApples(FruitSeller& seller, int money) {
+		numOfApples += seller.SaleApples(money);
+		myMoney -= money;
+	}
+	void ShowBuyResult() {
+		cout << "Left Money: " << myMoney << endl;
+		cout << "Apples: " << numOfApples << endl << endl;
+	}
+};
+
+int main() {
+	FruitSeller seller;
+	seller.InitMembers(1000, 20, 0);
+	FruitBuyer buyer;
+	buyer.InitMembers(5000);
+	buyer.BuyApples(seller, 2000);
+
+	cout << "Status of Seller" << endl;
+	seller.ShowSalesResult();
+	cout << "Status of Buyer" << endl;
+	buyer.ShowBuyResult();
+	return 0;
+}
+
+// p.144
